68c19a34c417d07946548a804dbd3db40d7d846b
Kedar Shashidhar <kedarshashi@gmail.com>
2016-09-10 11:47:24 -0700

    Adding audio.cs

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Audio : MonoBehaviour
+{
+    public static Audio clip;
+
+    [System.Serializable]
+    public struct NamedClip
+    {
+        public string name;
+        public AudioClip clip;
+    }
+    public NamedClip[] clips;
+
+    void Start ()
+    {
+        clip = this;    
+	}
+
+    public static string Convert(string s)
+    {
+        return s;
+    }
+}
