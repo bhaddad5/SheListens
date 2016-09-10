4a721920b3e8336c707bafa56c2536058a2711d4
Kedar Shashidhar <kedarshashi@gmail.com>
2016-09-10 01:37:36 -0700

    Added wwise integration

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour {
+
+    private Transform playerHead;
+
+	// Use this for initialization
+	void Start () {
+        playerHead = Camera.main.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
