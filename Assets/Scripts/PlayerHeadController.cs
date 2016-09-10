4cfa721d62be2f4a1158102f00ebf95f9b377f68
bhaddad5 <bhaddad5@gmail.com>
2016-09-10 11:44:36 -0700

    new controller scripts added

---
+++ Assets/Scripts/PlayerHeadController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHeadController : MonoBehaviour {
+
+	public AudioController audioController;
+	public GameStateController gameStateController;
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Trigger Enter");
+		if (other.GetComponent<GroundSoundController>())
+		{
+			audioController.currFloorType = other.GetComponent<GroundSoundController>().floorType;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		audioController.currFloorType = AudioController.currentFloorType.Wood;
+	}
+}
