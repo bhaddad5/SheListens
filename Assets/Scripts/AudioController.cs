18d99eddbceafeb1611661f5fd9e467ca0424c70
bhaddad5 <bhaddad5@gmail.com>
2016-09-10 01:24:50 -0700

    correct project setup

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class AudioController : MonoBehaviour {
 
+    private Transform playerHead;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        playerHead = Camera.main.transform;
+    }
 	
 	// Update is called once per frame
 	void Update () {
