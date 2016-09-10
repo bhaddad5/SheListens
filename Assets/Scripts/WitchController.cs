f00d2fb055f844c62a340e441e19497973794a45
bhaddad5 <bhaddad5@gmail.com>
2016-09-10 10:09:12 -0700

    witch arcs around player.

--- Assets/Scripts/WitchController.cs
+++ Assets/Scripts/WitchController.cs
@@ -3,29 +3,74 @@ using System.Collections;
 
 public class WitchController : MonoBehaviour {
 
-	private float maxWitchDistance = 3.0f;
+	private float maxWitchDistanceFromPlayer = 3.0f;
 	private float moveAwayFromPlayerSpeed = -0.003f;
 	private float moveTowardsPlayerSpeedModifier = 1f;
+	private float minWitchDirectionTime = 6.0f;
+	private float maxWitchDirectionTime = 10.0f;
+	private float minWitchArcSpeed = 0.002f;
+	private float maxWitchArcSpeed = 0.007f;
+	private float fixedWitchHeight = 0.6f;
 
-	private float currentNoiseLevel = 0;
+	private float currentNoiseLevel = 0f;
+	private Vector3 currentWitchPos;
+	private float lastChangedDirTime = 0f;
+	private float currentWitchArcTime = 0f;
+	private float currentWitchArcSpeed = 0f;
+	private float currentWitchArcDir = 1.0f;
 
 	// Update is called once per frame
 	void Update () {
-		if(currentNoiseLevel == 0)
+
+
+		currentWitchPos = UpdateWitchNoiseMovement();
+
+		updateWitchPosition();
+
+		UpdateWitchRotationAroundPlayer();
+	}
+
+	private Vector3 UpdateWitchNoiseMovement()
+	{
+		Vector3 witchNoiseMocement = new Vector3();
+
+		if (currentNoiseLevel == 0)
 		{
-			if(Vector3.Magnitude(transform.position - Camera.main.transform.position) < maxWitchDistance)
+			if (Vector3.Magnitude(transform.position - Camera.main.transform.position) < maxWitchDistanceFromPlayer)
 			{
-				transform.position = Vector3.MoveTowards(transform.position, Camera.main.transform.position, moveAwayFromPlayerSpeed);
+				witchNoiseMocement = Vector3.MoveTowards(transform.position, Camera.main.transform.position, moveAwayFromPlayerSpeed);
 			}
 			else
 			{
-				transform.position = Vector3.MoveTowards(transform.position, Camera.main.transform.position, -moveAwayFromPlayerSpeed);
+				witchNoiseMocement = Vector3.MoveTowards(transform.position, Camera.main.transform.position, -moveAwayFromPlayerSpeed);
 			}
 		}
 		else
 		{
-			transform.position = Vector3.MoveTowards(transform.position, Camera.main.transform.position, currentNoiseLevel * moveTowardsPlayerSpeedModifier);
+			witchNoiseMocement = Vector3.MoveTowards(transform.position, Camera.main.transform.position, currentNoiseLevel * moveTowardsPlayerSpeedModifier);
 		}
+
+		return witchNoiseMocement;
+	}
+
+	private void UpdateWitchRotationAroundPlayer()
+	{
+		if(Time.time - currentWitchArcTime > lastChangedDirTime)
+		{
+			lastChangedDirTime = Time.time;
+			currentWitchArcTime = Random.Range(minWitchDirectionTime, maxWitchDirectionTime);
+			currentWitchArcDir = currentWitchArcDir * -1;
+
+			currentWitchArcSpeed = Random.Range(minWitchArcSpeed, maxWitchArcSpeed) * currentWitchArcDir;
+		}
+
+		transform.RotateAround(Camera.main.transform.position, Vector3.up, currentWitchArcSpeed * 40f);
+	}
+
+	private void updateWitchPosition()
+	{
+		currentWitchPos.y = fixedWitchHeight;
+		transform.position = currentWitchPos;
 	}
 
 	public void updateNoiseLevel(float playerNoise)
