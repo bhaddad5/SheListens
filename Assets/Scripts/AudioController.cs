ba290bfd3124dfeabf972e12b2e3276cd7a1e73b
bhaddad5 <bhaddad5@gmail.com>
2016-09-10 02:24:49 -0700

    Witch follows player based on sound made.

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -3,15 +3,53 @@ using System.Collections;
 
 public class AudioController : MonoBehaviour {
 
-    private Transform playerHead;
+	private float playerHeadNoiseSpeedCutoff = 0.2f;
+	private float candleNoiseSpeedCutoff = 0.1f;
+	public Transform candle;
+	public WitchController Witch;
+
+	private float totalPlayerNoise;
+	private Vector3 prevCandlePos;
+	private Transform playerHead;
+	private Vector3 prevPlayerPos;
+	
+
+	public enum currentFloorType
+	{
+		Wood,
+		Glass,
+		Carpet
+	}
+	public currentFloorType currFloorType = currentFloorType.Wood;
 
 	// Use this for initialization
 	void Start () {
-        playerHead = Camera.main.transform;
-    }
-	
+		playerHead = Camera.main.transform;
+		prevPlayerPos = playerHead.position;
+		prevCandlePos = candle.position;
+	}
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update()
+	{
+		totalPlayerNoise = 0;
+
+		float headMoveDist = Vector3.Magnitude(playerHead.position - prevPlayerPos);
+		if (headMoveDist >= playerHeadNoiseSpeedCutoff)
+		{
+			Debug.Log("Playing head sound/volume for speed: " + headMoveDist + ", on " + currFloorType);
+			totalPlayerNoise += headMoveDist;
+		}
+		prevPlayerPos = playerHead.position;
+
+		float candleMoveDist = Vector3.Magnitude(candle.position - prevCandlePos);
+		if (candleMoveDist >= candleNoiseSpeedCutoff)
+		{
+			Debug.Log("Playing candle sound/volume for speed: " + candleMoveDist);
+			totalPlayerNoise += candleMoveDist;
+		}
+		prevCandlePos = candle.position;
+
+		Witch.updateNoiseLevel(totalPlayerNoise);
 	}
 }
---
+++ Assets/Scripts/WitchController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WitchController : MonoBehaviour {
+
+	private float maxWitchDistance = 3.0f;
+	private float moveAwayFromPlayerSpeed = -0.003f;
+	private float moveTowardsPlayerSpeedModifier = 0.1f;
+
+	private float currentNoiseLevel = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if(currentNoiseLevel == 0)
+		{
+			if(Vector3.Magnitude(transform.position - Camera.main.transform.position) < maxWitchDistance)
+			{
+				transform.position = Vector3.MoveTowards(transform.position, Camera.main.transform.position, moveAwayFromPlayerSpeed);
+			}
+			else
+			{
+				transform.position = Vector3.MoveTowards(transform.position, Camera.main.transform.position, -moveAwayFromPlayerSpeed);
+			}
+		}
+		else
+		{
+			transform.position = Vector3.MoveTowards(transform.position, Camera.main.transform.position, currentNoiseLevel * moveTowardsPlayerSpeedModifier);
+		}
+	}
+
+	public void updateNoiseLevel(float playerNoise)
+	{
+		currentNoiseLevel = playerNoise;
+	}
+}
