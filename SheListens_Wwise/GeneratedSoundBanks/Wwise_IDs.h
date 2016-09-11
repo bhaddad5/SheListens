026133cea3f6f94254f1935d1551d443cdea907c
Kedar Shashidhar <kedarshashi@gmail.com>
2016-09-11 14:50:13 -0700

    Added event for Key pick up and Blip

--- SheListens_Wwise/GeneratedSoundBanks/Wwise_IDs.h
+++ SheListens_Wwise/GeneratedSoundBanks/Wwise_IDs.h
@@ -18,6 +18,9 @@ namespace AK
         static const AkUniqueID PLAY_CREAKS = 1247850169U;
         static const AkUniqueID PLAY_DOOROPEN = 3197300806U;
         static const AkUniqueID PLAY_FOOTSTEP = 1602358412U;
+        static const AkUniqueID PLAY_GLASS_SMASH = 727285159U;
+        static const AkUniqueID PLAY_KEYBLIP = 3353471726U;
+        static const AkUniqueID PLAY_KEYPICKUP = 885077343U;
         static const AkUniqueID PLAY_LIGHTBULB_BUZZ = 3859524437U;
         static const AkUniqueID PLAY_LIGHTBULB_METAL = 1449975651U;
         static const AkUniqueID PLAY_LIGHTBULB_ZAP = 1785283739U;
