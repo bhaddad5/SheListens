da1b5a65947745c30fd3305ef1250fdf942ef049
Kedar Shashidhar <kedarshashi@gmail.com>
2016-09-11 10:10:39 -0700

    Integrated Ambience and Music

--- SheListens_Wwise/GeneratedSoundBanks/Wwise_IDs.h
+++ SheListens_Wwise/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,10 +13,32 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_BASELAYER = 4042215132U;
         static const AkUniqueID PLAY_CREAKS = 1247850169U;
         static const AkUniqueID PLAY_FOOTSTEP = 1602358412U;
+        static const AkUniqueID PLAY_MUSICENEMY = 786657857U;
         static const AkUniqueID PLAY_TEST = 3187507146U;
+        static const AkUniqueID PLAY_WITCH_HUNTING = 3978663595U;
+        static const AkUniqueID PLAY_WITCH_MOANS = 4038978142U;
+        static const AkUniqueID PLAY_WITCH_SCREAM = 3058690321U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace WITCH
+        {
+            static const AkUniqueID GROUP = 760583530U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ATTACKING = 1641806523U;
+                static const AkUniqueID GAMEOVER = 4158285989U;
+                static const AkUniqueID HUNTING = 1225919746U;
+                static const AkUniqueID IDLE = 1874288895U;
+            } // namespace STATE
+        } // namespace WITCH
+
+    } // namespace STATES
 
     namespace SWITCHES
     {
@@ -37,6 +59,7 @@ namespace AK
     namespace GAME_PARAMETERS
     {
         static const AkUniqueID HEADSPEED = 1893836608U;
+        static const AkUniqueID WITCHDIST = 4007242070U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
