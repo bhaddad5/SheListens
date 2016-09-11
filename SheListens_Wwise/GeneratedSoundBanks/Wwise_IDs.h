04880acaf13b521c6bf31f02bed69d98c026a7a0
Kedar Shashidhar <kedarshashi@gmail.com>
2016-09-11 13:19:07 -0700

    New GlassBreak Event

--- SheListens_Wwise/GeneratedSoundBanks/Wwise_IDs.h
+++ SheListens_Wwise/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,17 +14,35 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_BASELAYER = 4042215132U;
+        static const AkUniqueID PLAY_CANDLEBURN = 921938950U;
         static const AkUniqueID PLAY_CREAKS = 1247850169U;
         static const AkUniqueID PLAY_FOOTSTEP = 1602358412U;
+        static const AkUniqueID PLAY_GLASSSMASH = 1228833390U;
+        static const AkUniqueID PLAY_LIGHTBULB_BUZZ = 3859524437U;
+        static const AkUniqueID PLAY_LIGHTBULB_METAL = 1449975651U;
+        static const AkUniqueID PLAY_LIGHTBULB_ZAP = 1785283739U;
         static const AkUniqueID PLAY_MUSICENEMY = 786657857U;
         static const AkUniqueID PLAY_TEST = 3187507146U;
         static const AkUniqueID PLAY_WITCH_HUNTING = 3978663595U;
         static const AkUniqueID PLAY_WITCH_MOANS = 4038978142U;
         static const AkUniqueID PLAY_WITCH_SCREAM = 3058690321U;
+        static const AkUniqueID PLAY_ZAPOFF = 1311566966U;
+        static const AkUniqueID PLAY_ZAPON = 1839672760U;
     } // namespace EVENTS
 
     namespace STATES
     {
+        namespace LIGHTS
+        {
+            static const AkUniqueID GROUP = 3192784746U;
+
+            namespace STATE
+            {
+                static const AkUniqueID OFF = 930712164U;
+                static const AkUniqueID ON = 1651971902U;
+            } // namespace STATE
+        } // namespace LIGHTS
+
         namespace WITCH
         {
             static const AkUniqueID GROUP = 760583530U;
@@ -58,6 +76,7 @@ namespace AK
 
     namespace GAME_PARAMETERS
     {
+        static const AkUniqueID CANDLESPEED = 204323119U;
         static const AkUniqueID HEADSPEED = 1893836608U;
         static const AkUniqueID WITCHDIST = 4007242070U;
     } // namespace GAME_PARAMETERS
